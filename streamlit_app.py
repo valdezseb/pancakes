2d0e7e6b9ebdeec44bb5c82cf9f55ea2510e65a0
valdezseb <111101968+valdezseb@users.noreply.github.com>
2023-07-26 09:38:55 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,4 @@
 import streamlit as st
-from streamlit.report_thread import get_report_ctx
-from streamlit.server.server import Server
-
-# Define a function to get the current session state
-def get_session():
-    session = get_report_ctx().session
-    session_id = session.id
-    session_info = Server.get_current()._session_info_by_id.get(session_id)
-    if session_info is None:
-        raise RuntimeError("Couldn't get session info")
-    return session, session_id, session_info
 
 # Define the layout of the data analysis page
 def data_analysis_page():
@@ -22,20 +11,17 @@ def chat_bot_page():
     st.title('Chat Bot Page')
     st.write('This is the chat bot page of my app.')
     # Add your chat bot code here
-    with session:
-        if st.button('Go back to Data Analysis'):
-            session.state.page = 'Data Analysis'
+    if st.button('Go back to Data Analysis'):
+        st.session_state.page = 'Data Analysis'
 
 # Define your main function
 def main():
-    session, session_id, session_info = get_session()
-
     # Define the state of your app
-    if 'page' not in session.state:
-        session.state.page = 'Data Analysis'
+    if 'page' not in st.session_state:
+        st.session_state.page = 'Data Analysis'
 
     # Define the different pages of your app
-    if session.state.page == 'Data Analysis':
+    if st.session_state.page == 'Data Analysis':
         # Define the contents of the data analysis page
         st.sidebar.title('Navigation')
         option = st.sidebar.selectbox('Select an option', ('Data Analysis', 'Chat Bot'))
@@ -43,9 +29,9 @@ def main():
         if option == 'Data Analysis':
             data_analysis_page()
         elif option == 'Chat Bot':
-            session.state.page = 'Chat Bot'
+            st.session_state.page = 'Chat Bot'
 
-    elif session.state.page == 'Chat Bot':
+    elif st.session_state.page == 'Chat Bot':
         chat_bot_page()
 
 # Call your main function to run the app
