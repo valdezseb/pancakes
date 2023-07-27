fc8cd75e5a25710e4c5270cba27d4bff3eabf988
valdezseb <111101968+valdezseb@users.noreply.github.com>
2023-07-27 08:25:06 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,11 +45,11 @@ def load_embedding():
 embeddings = load_embedding()
 
 def load_pinecone(embeddings, index_name):
-    docsearch = Pinecone.from_existing_index(embeddings, index_name)
+    docsearch = Pinecone.from_existing_index(index_name, embeddings)
     return docsearch
 
 # Load the Pinecone client using st.cache
-docsearch = load_pinecone(embeddings, index_name)
+docsearch = load_pinecone(embeddings, "db-paseg")
 
 # Define the layout of the data analysis page
 def data_analysis_page():
@@ -63,7 +63,7 @@ def chat_bot_page():
     st.write('This is the chat bot page of my app.')
 
     # Load Pinecone and create Chat and RetrievalQA objects
-    #docsearch = load_pinecone(embeddings,index_name)
+    docsearch = load_pinecone("db-paseg")
     chat = ChatOpenAI(model_name='gpt-3.5-turbo-0613', temperature=0.80)
     qachain = load_qa_chain(chat, chain_type='stuff')
     qa = RetrievalQA(combine_documents_chain=qachain, retriever=docsearch.as_retriever())
@@ -73,16 +73,14 @@ def chat_bot_page():
         condition1 = '\n [Generate Response/Text from my data.]  \n [organize information: organize text so its easy to read, and bullet points when needed.] \n [if applicable for the question response, add section: Things to Promote/Things to Avoid and Best Practices, give Examples] \n [tone and voice style: clear sentences, avoid use of complex sentences]'
         # Let the user input a query
         user_query = st.text_input("Enter your query:")
-        # Display the button
-        if st.button("Run Query"):
-            # Run the QA system and display the result using Streamlit
-            result = qa.run(user_query + '\n' + condition1)
-            st.write(result)
+        # Run the QA system and display the result using Streamlit
+        result = qa.run(user_query + '\n' + condition1)
+        st.write(result)
 
     st.title("PASEG Genie // for education purpose :coffee:")
     st.markdown("*Chat With The Planning and Schedule Excellence Guide ver. 5.0*", unsafe_allow_html=True)
     st.markdown("---")
-    # Let the user input a query and run the query
+    # Let the user input a query
     run_query()
 
 # Define your main function
